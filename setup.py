74108e7615cc2c1d0d67c9b8aba6d971ee740bff
Juan S. Bokser <juan.bokser@gmail.com>
2020-10-26 15:26:14 -0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from easy_chain import __version__ as version
+
+with open("README.md", "r") as file_:
+    long_description = file_.read()
+
+with open("requirements.txt", "r") as file_:
+    requirements = file_.read().split()
+
+setup(
+    name='price_source',
+    version=version,
+    packages=find_packages(),
+    author='Juan S. Bokser',
+    author_email='juan.bokser@moneyonchain.com',
+    description='Prices source for MoC projects',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
+    ],
+    package_data={
+        "easy_chain": ["data/*.json"]
+    },
+    python_requires='>=3.6',
+    install_requires=requirements,
+    scripts=['moc_prices_source_check']
+)
