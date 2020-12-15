b8295ad490243561e7103fa7c4313a5094f4f915
Juan S. Bokser <juan.bokser@gmail.com>
2020-12-15 10:12:58 -0300

    Add RSK as a price source

---
+++ moc_prices_source/engines/rif_btc_rsk_two.py
@@ -0,0 +1,20 @@
+from engine_base import Base, RIF_BTC
+
+
+class Engine(Base):
+
+    _name        = Base._name_from_file(__file__)
+    _description = "RSK_2"
+    _uri         = "http://134.209.68.142:3000"
+    _coinpair    = RIF_BTC
+
+    def _map(self, data):
+        return {
+            'price': data['price']}
+
+
+if __name__ == '__main__':
+    print("File: {}, Ok!".format(repr(__file__)))
+    engine = Engine()
+    engine()
+    print(engine)
