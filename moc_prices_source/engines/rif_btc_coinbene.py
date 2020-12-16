a38206acf2bcb75a184b73016379e725b35c0179
Juan S. Bokser <juan.bokser@gmail.com>
2020-12-16 19:02:42 -0300

    FIX RIF/BTC Coinbene engine

--- moc_prices_source/engines/rif_btc_coinbene.py
+++ moc_prices_source/engines/rif_btc_coinbene.py
@@ -5,13 +5,15 @@ class Engine(Base):
 
     _name        = Base._name_from_file(__file__)
     _description = "Coinbene"
-    _uri         = "http://api.coinbene.com/v1/market/ticker?symbol=RIFBTC"
+    _uri         = "https://openapi-exchange.coinbene.com/api/exchange/v2/market/ticker/one?symbol=RIF%2FBTC"
+
     _coinpair    = RIF_BTC
 
     def _map(self, data):
         return {
-            'price':  data['ticker'][0]['last'],
-            'volume': data['ticker'][0]['24hrVol'] }
+            'price':  data['data']['latestPrice'],
+            'volume': data['data']['volume24h'] }
+
 
 
 if __name__ == '__main__':
