6688b33b82566cc354626279873ac62fdef58057
Juan S. Bokser <juan.bokser@gmail.com>
2022-07-06 18:19:21 -0300

    Add dolarito.ar engine for ARS/USD coinpair

---
+++ moc_prices_source/engines/ars_usd_dolarito.py
@@ -0,0 +1,27 @@
+from engine_base import Base, ARS_USD
+from decimal     import Decimal
+
+
+class Engine(Base):
+
+    _name        = Base._name_from_file(__file__)
+    _description = "Dolarito.ar"
+    _uri         = "https://www.dolarito.ar/api/frontend/quotations"
+    _coinpair    = ARS_USD
+    #_max_age     = 30
+
+    def _map(self, data):
+        info = data['informal']
+        values = [info['buy'], info['sell']]
+        values = list(map(lambda x: Decimal(str(x).replace('.', '').replace(',', '.')), values))
+        value = sum(values)/len(values)
+        return {
+            'price':  value
+        }
+
+
+if __name__ == '__main__':
+    print("File: {}, Ok!".format(repr(__file__)))
+    engine = Engine()
+    engine()
+    print(engine)
