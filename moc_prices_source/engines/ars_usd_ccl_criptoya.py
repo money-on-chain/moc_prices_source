d5faaad617e3d8e4a584b38914bd458001d2e0e2
Juan S. Bokser <juan.bokser@gmail.com>
2022-07-06 12:11:50 -0300

    Add CriptoYa.com engine for ARS/USD coinpair

---
+++ moc_prices_source/engines/ars_usd_ccl_criptoya.py
@@ -0,0 +1,21 @@
+from engine_base import Base, ARS_USD_CCL
+
+
+class Engine(Base):
+
+    _name        = Base._name_from_file(__file__)
+    _description = "CriptoYa.com"
+    _uri         = "https://criptoya.com/api/dolar"
+    _coinpair    = ARS_USD_CCL
+
+    def _map(self, data):
+        return {
+            'price':  data['ccl']
+        }
+
+
+if __name__ == '__main__':
+    print("File: {}, Ok!".format(repr(__file__)))
+    engine = Engine()
+    engine()
+    print(engine)
