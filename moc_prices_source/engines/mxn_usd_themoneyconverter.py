97dacaf8934dd5a596ac24399b69ac1ff2c1ff4f
Juan S. Bokser <juan.bokser@gmail.com>
2022-07-26 14:32:33 -0300

    FIX Remove some lost print() in code

--- moc_prices_source/engines/mxn_usd_themoneyconverter.py
+++ moc_prices_source/engines/mxn_usd_themoneyconverter.py
@@ -15,7 +15,6 @@ class Engine(EngineWebScraping):
     def _scraping(self, html):
         value = None
         for s in html.find_all ('div', id="cc-ratebox"):
-            print()
             d = s.string.strip().split()
             if len(d)==3 and d[0]=="MXN/USD" and d[1]=="=":
                 try:
