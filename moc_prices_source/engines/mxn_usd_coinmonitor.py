3ec224ae7f63007827b7ca195e111a6d6d163096
Juan S. Bokser <juan.bokser@gmail.com>
2022-08-03 09:21:45 -0300

    Add CoinMonitor.com Engine for ARS/USD and MXN/USD coinpairs

---
+++ moc_prices_source/engines/mxn_usd_coinmonitor.py
@@ -0,0 +1,26 @@
+from engine_base import Base, MXN_USD
+
+
+class Engine(Base):
+
+    _name        = Base._name_from_file(__file__)
+    _description = "CoinMonitor.info"
+    _uri         = "https://mx.coinmonitor.info/data_ar_chart_DOLAR.json"
+    _coinpair    = MXN_USD
+    
+    _max_age                       = 3600 # 1hs.
+    _max_time_without_price_change = 0    # zero means infinity
+
+    def _map(self, data):
+        return {
+            'price':  data[0][1]
+        }
+
+
+if __name__ == '__main__':
+    print("File: {}, Ok!".format(repr(__file__)))
+    engine = Engine()
+    engine()
+    print(engine)
+    if engine.error:
+        print(engine.error)
