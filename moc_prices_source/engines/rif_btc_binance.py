9aba9ddc0464bcd9e6c97c89da7011e0d3d090c6
Juan S. Bokser <juan.bokser@gmail.com>
2022-12-28 10:31:20 -0300

    FIX bad uri

--- moc_prices_source/engines/rif_btc_binance.py
+++ moc_prices_source/engines/rif_btc_binance.py
@@ -7,7 +7,7 @@ class Engine(BaseWithFailover):
     _name         = BaseWithFailover._name_from_file(__file__)
     _description  = "Binance"
     _uri          = base_uri.format("api.binance.com")
-    _uri_failover = base_uri.format("proxy-api-binance.moneyonchain.com")
+    _uri_failover = base_uri.format("moc-proxy-api-binance.moneyonchain.com")
     _coinpair     = RIF_BTC
     _max_time_without_price_change = 0 # zero means infinity
 
@@ -21,5 +21,11 @@ if __name__ == '__main__':
     print("File: {}, Ok!".format(repr(__file__)))
     engine = Engine()
     engine()
-    print(engine.uri)
+    print(f"URI = {repr(engine.uri)}")
+    print()
     print(engine)
+    print()
+    if engine.error:
+        print()
+        print(engine.error)
+        print()
