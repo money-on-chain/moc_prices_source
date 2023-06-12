ebd37272a2fcee64462f1ab2c6920bb4e2dff3d0
Juan S. Bokser <juan.bokser@gmail.com>
2023-06-12 20:17:01 -0300

    improvements on web3 engines

--- moc_prices_source/engines/rif_btc_sovryn.py
+++ moc_prices_source/engines/rif_btc_sovryn.py
@@ -1,4 +1,4 @@
-from engine_base import BaseOnChain, RIF_BTC
+from engine_base import BaseOnChain, RIF_BTC, get_env
 
 
 
@@ -7,20 +7,20 @@ class Engine(BaseOnChain):
     _name          = BaseOnChain._name_from_file(__file__)
     _description   = "Sovryn onchain"
     _coinpair      = RIF_BTC
-    _uri           = 'https://public-node.rsk.co'
+    _uri           = get_env('RSK_NODE', 'https://public-node.rsk.co')
     _pool_sc_addr  = '0x65528e06371635a338ca804cd65958a11cb11009'
     _wrbtc_tk_addr = '0x542fda317318ebf1d3deaf76e0b632741a7e677d'
     _rif_tk_addr   = '0x2acc95758f8b5f583470ba265eb685a8f45fc9d5'
 
     def _get_price(self):
 
-        try:
-            
-            pool_sc_addr = self.Web3.toChecksumAddress(self._pool_sc_addr)
-            wrbtc_tk_addr = self.Web3.toChecksumAddress(self._wrbtc_tk_addr)
-            rif_tk_addr = self.Web3.toChecksumAddress(self._rif_tk_addr)
+        pool_sc_addr = self.to_checksum_address(self._pool_sc_addr)
+        wrbtc_tk_addr = self.to_checksum_address(self._wrbtc_tk_addr)
+        rif_tk_addr = self.to_checksum_address(self._rif_tk_addr)
 
-            w3 = self.Web3(self.HTTPProvider(self._uri))
+        try:            
+
+            w3 = self.make_web3_obj_with_uri()
 
             rif_token = w3.eth.contract(address=rif_tk_addr, abi=self.erc20_simplified_abi)
             wrbtc_token = w3.eth.contract(address=wrbtc_tk_addr, abi=self.erc20_simplified_abi)
