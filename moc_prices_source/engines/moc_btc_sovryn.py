58e4b50bc9cdf478db8e9ba069fe3d13ed86b8d7
Juan S. Bokser <juan.bokser@gmail.com>
2023-07-12 12:49:56 -0300

    fix: moc/btc from sovryn

--- moc_prices_source/engines/moc_btc_sovryn.py
+++ moc_prices_source/engines/moc_btc_sovryn.py
@@ -1,4 +1,4 @@
-from engine_base import BaseOnChain, MOC_BTC
+from engine_base import BaseOnChain, MOC_BTC, get_env
 
 
 
@@ -7,20 +7,20 @@ class Engine(BaseOnChain):
     _name          = BaseOnChain._name_from_file(__file__)
     _description   = "Sovryn onchain"
     _coinpair      = MOC_BTC
-    _uri           = 'https://public-node.rsk.co'
+    _uri           = get_env('RSK_NODE', 'https://public-node.rsk.co')
     _pool_sc_addr  = '0xe321442dc4793c17f41fe3fb192a856a4864ceaf'
     _wrbtc_tk_addr = '0x542fda317318ebf1d3deaf76e0b632741a7e677d'
     _moc_tk_addr   = '0x9ac7fe28967b30e3a4e6e03286d715b42b453d10'
 
     def _get_price(self):
 
+        pool_sc_addr = self.to_checksum_address(self._pool_sc_addr)
+        wrbtc_tk_addr = self.to_checksum_address(self._wrbtc_tk_addr)
+        moc_tk_addr = self.to_checksum_address(self._moc_tk_addr)
+
         try:
             
-            pool_sc_addr = self.Web3.toChecksumAddress(self._pool_sc_addr)
-            wrbtc_tk_addr = self.Web3.toChecksumAddress(self._wrbtc_tk_addr)
-            moc_tk_addr = self.Web3.toChecksumAddress(self._moc_tk_addr)
-
-            w3 = self.Web3(self.HTTPProvider(self._uri))
+            w3 = self.make_web3_obj_with_uri()
 
             moc_token = w3.eth.contract(address=moc_tk_addr, abi=self.erc20_simplified_abi)
             wrbtc_token = w3.eth.contract(address=wrbtc_tk_addr, abi=self.erc20_simplified_abi)
