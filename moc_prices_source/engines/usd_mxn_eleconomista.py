60724ce0df1afa4479b682501c3838a432ec1dd1
Juan S. Bokser <juan.bokser@gmail.com>
2024-01-31 17:32:09 -0300

    fix: 403 client error forbidden for el economista

--- moc_prices_source/engines/usd_mxn_eleconomista.py
+++ moc_prices_source/engines/usd_mxn_eleconomista.py
@@ -12,6 +12,8 @@ class Engine(EngineWebScraping):
     _max_age                       = 3600 # 1hs.
     _max_time_without_price_change = 0    # zero means infinity
 
+    _headers = {'User-agent': 'Mozilla/5.0'} # FIX: 403 Client Error Forbidden
+
     def _scraping(self, html):
         value = None
         for s in html.find_all('span', class_="ultimo_21334 last-value" ):
