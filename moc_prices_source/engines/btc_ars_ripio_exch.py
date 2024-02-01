1efb88619efd254b91cc44c0a84781f4c0afd832
Juan S. Bokser <juan.bokser@gmail.com>
2024-02-01 09:14:53 -0300

    fix: ripio exchange btc_ars engine

--- moc_prices_source/engines/btc_ars_ripio_exch.py
+++ moc_prices_source/engines/btc_ars_ripio_exch.py
@@ -1,11 +1,12 @@
 from engine_base import Base, BTC_ARS
+from decimal import Decimal
 
 
 class Engine(Base):
 
     _name        = Base._name_from_file(__file__)
     _description = "Ripio exchange"
-    _uri         = "https://api.exchange.ripio.com/api/v1/rate/BTC_ARS"
+    _uri         = "https://api.ripiotrade.co/v4/public/tickers/BTC_ARS"
     _coinpair    = BTC_ARS
     
     _max_age                       = 3600 # 1hs.
@@ -13,8 +14,8 @@ class Engine(Base):
 
     def _map(self, data):
         return {
-            'price': data['last_price'],
-            'volume': data['volume']
+            'price': (Decimal(data['data']['ask']) + Decimal(data['data']['bid'])) / Decimal('2'),
+            'volume': data['data']['volume']
         }
 
 
