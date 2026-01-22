d6b499718877b66e1a1690b6aff1c507bbe6aa82
Juan S. Bokser <juan.bokser@gmail.com>
2026-01-22 18:22:28 -0300

    feat: adds scaffolding for microliquidations

--- moc_prices_source/plugins/pairs_computed_onchain.py
+++ moc_prices_source/plugins/pairs_computed_onchain.py
@@ -1,8 +1,11 @@
 from .chains import chain, EVM
 from .base import CoinPair, Formula, register_pairs
-from .pairs import BTC_USD
+from .pairs import BTC_ARS, BTC_COP, BTC_USD
+from .pairs_onchain import BPRO_BTC
 from .base import get_addr_env, Decimal
-from ..types import PercentageDecimal
+from ..types import PercentageDecimal, Yes, No
+from ..evm import addr_zero
+import random
 
 
 
@@ -50,6 +53,89 @@ BTC_USD_24h = CoinPair(
     short_description = "test",
     requirements = [BTC_USD],
     formula = BTC_USD_24h_Formula)
+
+
+requirements_flip = [BTC_ARS, BTC_COP, BTC_USD, BPRO_BTC]
+
+class ISLIQ_FLIP_Formula(Formula):
+    """
+        MultiCollateralGuard.readyToLiquidate([
+            [bpro_ars, bpro_cop],
+            [usd_ars, usd_cop]
+        ])
+    """
+
+    evm: EVM = chain.rsk_mainnet.evm
+    mcg_addr = get_addr_env(
+        'MULTI_COLLATERAL_GUARD_ADDR',
+        addr_zero)
+    
+    max_steps = 3
+
+    def step_run(self, value, step, btc_ars, btc_cop, btc_usd, bpro_btc):
+        if step==1:
+            
+            wei = lambda value: int(value * Decimal("1e18"))
+
+            usd_ars = wei(btc_ars / btc_usd)
+            usd_cop = wei(btc_cop / btc_usd)
+            bpro_ars =  wei(bpro_btc * btc_ars)
+            bpro_cop = wei(bpro_btc * btc_cop)
+            
+            ...
+            fn_spec = 'readyToLiquidate(uint256[][])(bool)' or \
+                'readyToMicroLiquidate(uint256[][])(bool)'
+            call_args = [
+                    [bpro_ars, bpro_cop],
+                    [usd_ars, usd_cop]
+                ]
+            self.call_id = self.evm.multicall.add_call(
+                self.mcg_addr, fn_spec, call_args)
+            ...
+            
+            self.evm.multicall.reset_executed_once()
+        elif step==2:
+            self.evm.multicall.run_only_first_time()
+            
+            ...
+            #self.out_value = self.evm.multicall.get_call(self.call_id)
+            self.out_value = random.randint(1, 10) == 1
+            ...
+            
+        else:
+            self.evm.multicall.clear_calls()
+            if self.out_value is None:
+                return None
+            return Yes if self.out_value else No
+
+
+ISLIQ_FLIP = CoinPair(
+    name="ISLIQ_FLIP",
+    short_description = "If FLip is in liquidation (mainnet)",
+    requirements = requirements_flip,
+    formula = ISLIQ_FLIP_Formula)
+
+
+class ISLIQ_FLIP_TEST_Formula(ISLIQ_FLIP_Formula):
+    """
+        MultiCollateralGuardTestnet.readyToLiquidate([
+            [bpro_ars, bpro_cop],
+            [usd_ars, usd_cop]
+        ])
+    """
+
+    evm: EVM = chain.rsk_testnet.evm
+    mcg_addr = get_addr_env(
+        'MULTI_COLLATERAL_GUARD_TESTNET_ADDR',
+        addr_zero)
+
+
+ISLIQ_FLIP_TEST = CoinPair(
+    name="ISLIQ_FLIP",
+    variant="test",
+    short_description = "If FLip is in liquidation (testnet)",
+    requirements = requirements_flip,
+    formula = ISLIQ_FLIP_TEST_Formula)
 
 
 register_pairs()
