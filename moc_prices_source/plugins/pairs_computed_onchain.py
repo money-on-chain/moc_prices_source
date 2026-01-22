f0e0b967506cbfda54b29975058fece67b931b90
Juan S. Bokser <juan.bokser@gmail.com>
2026-01-22 15:22:07 -0300

    fix: adding BTC_USD_24h as an example pair; it can be removed later.

---
+++ moc_prices_source/plugins/pairs_computed_onchain.py
@@ -0,0 +1,53 @@
+from .chains import chain, EVM
+from .base import CoinPair, Formula, register_pairs
+from .pairs import BTC_USD
+from .base import get_addr_env, Decimal
+from ..types import PercentageDecimal
+
+
+
+### Computed onchain pairs
+
+class BTC_USD_24h_Formula(Formula):
+
+    max_steps = 2
+    evm: EVM = chain.rsk_mainnet.evm
+    oracle_addr = get_addr_env('BTC_USD_ORACLE_ADDR',
+                               '0xe2927A0620b82A66D67F678FC9b826B0E01B1bFD')
+    coinpair = BTC_USD
+    hours: int = 24
+
+    __doc__ = (f"({coinpair}@NOW - {coinpair}@{hours}hAGO)"
+               f" / {coinpair}@{hours}hAGO")
+
+    def step_run(self, value, step, btc_usd):
+        if step==1:
+            self.block = self.evm.latest_block_number - int(
+                3600 * self.hours / 25)
+            self.call_id = self.evm.multicall.add_call(
+                contract_address = self.oracle_addr,
+                fn_spec = 'peek()(bytes32,bool)')
+        else: # final step
+            namespace = f"{self.hours}h ago"
+            self.evm.multicall.run_only_first_time(
+                block_identifier = self.block,
+                namespace = namespace)
+            value_b, ok = self.evm.multicall.get_call(
+                self.call_id, namespace = namespace)
+            if ok:
+                btc_usd_before = Decimal(int(value_b.hex(), 16)
+                                         )/Decimal(10**18)
+            else:
+                raise ValueError('invalid or expired price')
+            self.evm.multicall.reset_executed_once(namespace=None)
+            return PercentageDecimal((btc_usd - btc_usd_before) / btc_usd_before)
+
+
+BTC_USD_24h = CoinPair(
+    name = "BTC/USD(24h)",
+    short_description = "test",
+    requirements = [BTC_USD],
+    formula = BTC_USD_24h_Formula)
+
+
+register_pairs()
