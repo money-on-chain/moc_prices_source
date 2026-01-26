613547b32b1b0cc454833326f4161d1ce60924ef
Juan S. Bokser <juan.bokser@gmail.com>
2026-01-26 11:30:03 -0300

    refactor: reorganize the engines into subfolders to simplify maintenance

---
+++ moc_prices_source/plugins/pairs/special.py
@@ -0,0 +1,25 @@
+from ..base import CoinPair, register_pairs
+from ..coins import BTC, DOC, RIF, USD, USDT
+
+
+
+# Special pairs
+
+#Rootstock block number
+BLOCK_RSK = CoinPair(name="BLOCK", variant="RSK",
+                     short_description = "Rootstock block number")
+
+# DOC/USD
+DOC_USD = CoinPair(DOC, USD, short_description="Pegged 1:1 to USD")
+
+# RIF/BTC
+RIF_BTC_MP1P = CoinPair(RIF, BTC, "mp1%",
+                        description = "To move the price 1 percent",
+                        short_description = "To move the price 1%")
+
+# RIF/USDT
+RIF_USDT_MP1P = CoinPair(RIF, USDT, "mp1%",
+                        description = "To move the price 1 percent",
+                        short_description = "To move the price 1%")
+
+register_pairs()
