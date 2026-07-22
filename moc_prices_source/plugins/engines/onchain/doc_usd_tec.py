3f83e25db9b8bb0769bf28e96e70d7920f455bfc
Juan S. Bokser <juan.bokser@gmail.com>
2026-07-22 15:41:00 -0300

    fix: Honor invalid DOC/USD oracle responses and Guard the testnet DOC engine when disabled

--- moc_prices_source/plugins/engines/onchain/doc_usd_tec.py
+++ moc_prices_source/plugins/engines/onchain/doc_usd_tec.py
@@ -1,15 +1,20 @@
 from ...pairs.onchain import DOC_USD_TEC
-from ...base import BaseOnChain, Engines, get_addr_env, EVM, Decimal
-from ...chains import chain
+from ...base import BaseOnChain, Engines, EVM, Decimal
+
+
 
 @Engines.register_decorator()
 class Engine(BaseOnChain):
-    per_env_evm: EVM = chain.rsk_mainnet.evm
-    price_provider_addr = '0x6a343488338b944c6FCc89906646Fac1e8e91cE5'
+
     _description = "DOC/USD onchain"
     _coinpair = DOC_USD_TEC
+    _addr = '0x6a343488338b944c6FCc89906646Fac1e8e91cE5'
 
     def _get_value_from_evm(self, evm: EVM):
-        (value_wei, _) = self.per_env_evm.call(self.price_provider_addr, 'peek()(bytes32,bool)')
-        value = Decimal(int.from_bytes(value_wei))/Decimal(10**18)
-        return value, None # (value, str_error)
+        value, str_error = None, None
+        value_b, ok = evm.call(self._addr, 'peek()(bytes32,bool)')
+        if ok:
+            value = Decimal(int(value_b.hex(), 16))/Decimal(10**18)
+        else:
+            str_error = 'invalid or expired price'
+        return value, str_error
--- moc_prices_source/plugins/engines/onchain/doc_usd_tec_test.py
+++ moc_prices_source/plugins/engines/onchain/doc_usd_tec_test.py
@@ -1,15 +1,23 @@
 from ...pairs.onchain import DOC_USD_TEC_TEST
-from ...base import BaseOnChain, Engines, get_addr_env, EVM, Decimal
+from ...base import BaseOnChain, Engines, EVM, Decimal
 from ...chains import chain
 
-@Engines.register_decorator()
-class Engine(BaseOnChain):
-    per_env_evm: EVM = chain.rsk_testnet.evm
-    price_provider_addr = '0xaeB119cF080FDD668E6Ba845f663912C473778F8'
-    _description = "DOC/USD onchain (testnet)"
-    _coinpair = DOC_USD_TEC_TEST
 
-    def _get_value_from_evm(self, evm: EVM):
-        (value_wei, _) = self.per_env_evm.call(self.price_provider_addr, 'peek()(bytes32,bool)')
-        value = Decimal(int.from_bytes(value_wei))/Decimal(10**18)
-        return value, None # (value, str_error)
+if chain.rsk_testnet.enabled: 
+
+    @Engines.register_decorator()
+    class Engine(BaseOnChain):
+
+        _evm: EVM = chain.rsk_testnet.evm
+        _description = "DOC/USD onchain (testnet)"
+        _coinpair = DOC_USD_TEC_TEST
+        _addr = '0xaeB119cF080FDD668E6Ba845f663912C473778F8'
+
+        def _get_value_from_evm(self, evm: EVM):
+            value, str_error = None, None
+            value_b, ok = evm.call(self._addr, 'peek()(bytes32,bool)')
+            if ok:
+                value = Decimal(int(value_b.hex(), 16))/Decimal(10**18)
+            else:
+                str_error = 'invalid or expired price'
+            return value, str_error
